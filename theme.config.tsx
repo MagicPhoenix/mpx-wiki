b2d0f1ea1f4c47d7df02500296a1dd543c5b34b0
Livezxy <47280313+Livezxy@users.noreply.github.com>
2024-01-16 02:33:48 +0800

    init

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>MPX WIKI</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://mpx-wiki.vercel.app/',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/3Uhq6k7YSg',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/MagicPhoenix/mpx-wiki',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'MPX Wiki',
   },
 }
 
